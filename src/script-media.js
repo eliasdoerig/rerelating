15417fab0456cb145a158a07b70fdf18a45f0104
Elias Dörig <27639750+eliasdoerig@users.noreply.github.com>
2021-12-16 13:44:22 +0100

    first commit

---
+++ src/script-media.js
@@ -0,0 +1,42 @@
+import { media } from "./media.json";
+
+const title = document.getElementById("title");
+const videoPlayer = document.getElementById("video");
+const audioPlayer = document.getElementById("audio");
+videoPlayer.style.display = "none";
+audioPlayer.style.display = "none";
+
+const init = function () {
+  const currentSearch = window.location.search.replace(/\?/g, "");
+  const currentMedia = media.find((e) => {
+    return e.slug === currentSearch;
+  });
+  if (currentMedia) {
+    title.innerText = currentMedia.title;
+    if (currentMedia.type === "audio/mp3") {
+      addSource(audioPlayer, currentMedia.src, currentMedia.type);
+      audioPlayer.load();
+      audioPlayer.style.display = "block";
+    } else if (currentMedia.type === "video/mp4") {
+      addSource(videoPlayer, currentMedia.src, currentMedia.type);
+      videoPlayer.load();
+      videoPlayer.style.display = "block";
+    } else {
+      title.innerText = `This media type is not supported: ${currentMedia.type}`;
+      console.error(`This media type is not supported: ${currentMedia.type}`);
+    }
+  } else {
+    title.innerHTML = `This media does not exist. <br><a href="index.html">< Go back</a>`;
+  }
+};
+
+const addSource = function (element, src, type) {
+  var source = document.createElement("source");
+
+  source.src = src;
+  source.type = type;
+
+  element.appendChild(source);
+};
+
+init();
---
+++ src/script.js
@@ -0,0 +1,70 @@
+import { media } from "./media.json";
+
+// Grab elements, create settings, etc.
+const video = document.getElementById("video");
+const title = document.getElementById("title");
+const searching = document.getElementById("searching");
+const mediaLink = document.getElementById("mediaLink");
+let isLinkVisible = false;
+let currentMedia = "";
+
+function init() {
+  // Create a webcam capture
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        facingMode: "environment",
+      },
+      audio: false,
+    })
+    .then((stream) => {
+      //track = stream.getTracks()[0];
+      video.srcObject = stream;
+    })
+    .then(() => {
+      ml5
+        .imageClassifier("test-image-model/model.json", video)
+        .then((classifier) => loop(classifier));
+    })
+    .catch(function (error) {
+      console.error("Oops. Something is broken.", error);
+    });
+  // Initialize the Image Classifier method with MobileNet passing the video as the
+  // second argument and the getClassification function as the third
+}
+
+const loop = (classifier) => {
+  classifier.classify().then((results) => {
+    const conf = results[0].confidence.toFixed(4);
+    //probability.innerText = `${conf * 100}%`;
+    if (conf > 0.95) {
+      if (!isLinkVisible) {
+        currentMedia = media.find((e) => {
+          return e.slug === results[0].label;
+        });
+        title.innerHTML = `${currentMedia.slug}<br>${currentMedia.title}`;
+        searching.style.display = "none";
+        mediaLink.style.opacity = 1;
+        isLinkVisible = true;
+      }
+    } else if (conf < 0.6 && isLinkVisible) {
+      if (isLinkVisible) {
+        currentMedia = "";
+        title.innerText = "";
+        searching.style.display = "block";
+        mediaLink.style.opacity = 0;
+        isLinkVisible = false;
+      }
+    }
+    loop(classifier); // Call again to create a loop
+  });
+};
+
+window.addEventListener("load", init, false);
+
+mediaLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (isLinkVisible && currentMedia !== "") {
+    window.location.href = `media.html?${currentMedia.slug}`;
+  }
+});
